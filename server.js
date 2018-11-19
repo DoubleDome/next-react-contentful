d77b87e9eebcbbba15d6cb77628c2d1a3e189b75
Cristian Abello <criabell@publicisgroupe.net>
2018-11-19 12:43:38 -0500

    blank line removed

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const next = require('next');
-
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({dir: './src' , dev });
 const handle = app.getRequestHandler();
