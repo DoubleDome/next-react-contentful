7df36f9e203e6ac9618eb04c1690107e5a6b6bff
Cristian Abello <criabell@publicisgroupe.net>
2018-11-20 08:42:20 -0500

    promo cards populate from contentful now

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,20 +16,30 @@ class Index extends React.Component {
     });
 
     {/* Todo: input validation, either here or Contentful */}
-    return { roomLandingPageProp: entries.items[0],
+    return {
         textHeader: entries.items[0].fields.textHeader.fields,
         hero: entries.items[0].fields.sectionHero.fields,
-        roomCollection: entries.items[0].fields.roomCollection,
+        roomCollection: entries.items[0].fields.roomCollection.map((room) => {
+          return room.fields;
+        }),
         roomCollectionLayout: entries.items[0].roomCollectionLayout,
         secondHero: entries.items[0].fields.premiumSectionHero.fields,
-        promoCards: entries.items[0].fields.promoCards,
+        promoSection: {
+          title: entries.items[0].fields.promoSectionTitle,
+          button: {
+            label: entries.items[0].fields.prromoSectionButtonLabel,
+            url: entries.items[0].fields.prromoSectionButtonUrl,
+          },
+          cards: entries.items[0].fields.promoCards.map((card) => {
+            return card.fields;
+          }),
+        }
     };
   }
 
   render() {
-    const content = this.props.roomLandingPageProp.fields;
 
-    const { textHeader, hero, roomCollection, roomCollectionLayout, secondHero, promoCards } = this.props;
+    const { textHeader, hero, roomCollection, roomCollectionLayout, secondHero, promoSection } = this.props;
 
     return (
       <Layout>
@@ -74,15 +84,14 @@ class Index extends React.Component {
         {/* GraphQL version of Hero will look more like this, much cleaner */}
         <G2RoomOverviewCardCollectionSection
             rooms={
-              roomCollection.map((entry) => {
-                  const room = entry.fields;
+              roomCollection.map((room) => {
                   return (
                   {
                       title: room.title,
                       keyValues: [room.squareFeet, room.bedType, room.maxGuests],
                       description: room.shortDescription.content[0].content[0].value,
                       image: {
-                        url: room.cardImageUrl,
+                        url: room.cardImageUrl
                       },
                       primaryAction: {
                         label: room.primaryActionLabel,
@@ -116,86 +125,32 @@ class Index extends React.Component {
 
         {/* TODO: this should pull from Contentful */}
         <PromoCardsRowSection
-          title={"Offers & Promos"}
-          readMoreButton={{ label: 'View All Offers', url: '/' }}
-          cards={[
-            {
-              imageUrl:
-                'https://static.mgmresorts.com/content/dam/MGM/aria/hotel/aria/exterior/aria-hotel-exterior-campus-forbes-announcement.tif',
-              categoryName: 'Offers',
-              title: 'Warm up with 30% off your winter escape',
-              description:
-                'Book by November 15 and stay by September 30, 2019 to enjoy these special rates.',
-              status: {
-                color: 'red',
-                label: 'Ends soon',
-              },
-              primaryAction: {
-                label: 'Book now',
-                url:
-                  'https://www2.aria.com/en/booking/room-booking.html#/default?programId=279188f0-2e78-4f54-a4c2-703a2c52d0e6&selectedPropertyId=e2704b04-d515-45b0-8afd-4fa1424ff0a8',
-              },
-              tertiaryAction: {
-                label: 'Learn More',
-                 url:
-                  'https://www2.aria.com/en/offers/offer-detail.html#/offerType=ROOM&offerId=279188f0-2e78-4f54-a4c2-703a2c52d0e6&programType=OpenLoop',
-              },
-            },
-            {
-              imageUrl:
-                'https://static.mgmresorts.com/content/dam/MGM/aria/hotel/resort-club-lounge/resort-club-lounge-rooms/resort-club-lounge-queen/aria-hotel-deluxe-resort-club-lounge-queen-room.jpg',
-              categoryName: 'Offers',
-              title: 'Fall Savings Package',
-              description:
-                'Book by November 15 to enjoy these special rates plus bonus discounts.',
-              primaryAction: {
-                label: 'Book offer',
-                url:
-                  'https://www2.aria.com/en/booking/room-booking.html#/default?programId=580b66b8-f4db-4777-b93a-43bc9c32feb9&selectedPropertyId=e2704b04-d515-45b0-8afd-4fa1424ff0a8',
-              },
-              tertiaryAction: {
-                label: 'Learn More',
-                url:
-                  'https://vacationsbymgmresorts.poweredbygps.com/g/pt/vacation-packages/?MDPCID=US.TPS.MGM.BRAND-ARIA.EPACKAGE&icid=DMP_Tile_BB_HO_EV_MGMVacations_09042018_LV_AR',
-              },
-            },
-            {
-              imageUrl:
-                'https://static.mgmresorts.com/content/dam/MGM/aria/corporate-initiatives/aria-hotel-resorts-vacations.tif',
-              categoryName: 'Offers',
-              title: 'Vacations by MGM Resorts',
-              description:
-                'Save more when you book your flight and hotel together!',
-              primaryAction: {
-                label: 'Book now',
-                url:
-                  'https://vacationsbymgmresorts.poweredbygps.com/g/pt/vacation-packages/?MDPCID=US.TPS.MGM.BRAND-ARIA.EPACKAGE&icid=DMP_Tile_BB_HO_EV_MGMVacations_09042018_LV_AR',
-              },
-              tertiaryAction: {
-                label: 'Learn More',
-                url:
-                  'https://vacationsbymgmresorts.poweredbygps.com/g/pt/vacation-packages/?MDPCID=US.TPS.MGM.BRAND-ARIA.EPACKAGE&icid=DMP_Tile_BB_HO_EV_MGMVacations_09042018_LV_AR',
-              },
-            },
-            {
-              imageUrl:
-                'https://static.mgmresorts.com/content/dam/MGM/aria/hotel/aria/exterior/lifestyle/aria-hotel-exterior-lifestyle-couple-evening-lumia.jpg',
-              categoryName: 'Offers',
-              title: 'Double the points, double the fun',
-              description:
-                'Earn double World of Hyatt Points on all eligible spend through December 29.',
-              primaryAction: {
-                label: 'Book now',
-                url:
-                  'https://world.hyatt.com/content/gp/en/wohpromotions/mgm-vegas-double-points-promotion.html',
-              },
-              tertiaryAction: {
-                label: 'Learn More',
-                url:
-                  'https://world.hyatt.com/content/gp/en/wohpromotions/mgm-vegas-double-points-promotion.html',
-              },
-            },
-          ]}
+          title={promoSection.title}
+          readMoreButton={{ label: promoSection.button.label, url: promoSection.button.url }}
+          cards={
+            promoSection.cards.map((card) => {
+              return (
+                {
+                  imageUrl: card.imageUrl,
+                  categoryName: card.categoryName,
+                  title: card.title,
+                  description: card.description,
+                  status: {
+                    color: card.statusColor,
+                    label: card.statusLabel,
+                  },
+                  primaryAction: {
+                    label: card.primaryActionLabel,
+                    url: card.primaryActionUrl,
+                  },
+                  tertiaryAction: {
+                    label: card.tertiaryActionLabel,
+                    url: card.tertiaryActionUrl,
+                  },
+                }
+              )
+            })
+          }
         />
 
       </Layout>
