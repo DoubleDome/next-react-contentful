ba38cbd9de606bd48d348d9fb8c3438598657010
Cristian Abello <cristian.abello@valpo.edu>
2018-11-27 19:34:07 +0000

    content restructure of roomLandingPage

--- next/pages/index.js
+++ next/pages/index.js
@@ -1,6 +1,5 @@
 import { createDeliveryClient } from '../integrations/contentful';
 import config from '../integrations/contentful/config';
-
 import React from 'react';
 import Layout from '../layouts/layout';
 import G2TextHeaderSection from '../../dmp/components/G2TextHeaderSection/G2TextHeaderSection.component';
@@ -8,89 +7,135 @@ import G2HeroSection from '../../dmp/components/G2HeroSection/G2HeroSection.comp
 import G2RoomOverviewCardCollectionSection from '../../dmp/components/G2RoomOverviewCardCollectionSection/G2RoomOverviewCardCollectionSection.component';
 import PromoCardsRowSection from '../../src/components/PromoCardsRowSection/PromoCardsRowSection.component';
 
+
 class Index extends React.Component {
   static async getInitialProps() {
+    let pageComponents;
     const client = createDeliveryClient(config.spaces.rooms);
-    const entries = await client.getEntries({
+    const content = await client.getEntries({
       content_type: 'roomLandingPage',
       include: 1,
+    }).then((res) => {
+        const entries = res.items[0].fields;
+        pageComponents = {
+          all: entries,
+          textHeader: {
+            title: entries.textHeaderTitle,
+            subtitle: entries.textHeaderSubtitle,
+          },
+          hero: {
+            premium: entries.firstSectionHeroPremium,
+            title: entries.firstSectionHeroTitle,
+            images: entries.firstSectionHeroRooms.map((entry) => {
+              const room = entry.fields;
+              return ({
+                url: room.cardImageUrl,
+                caption: 'needs content field',
+                tertiaryAction: {
+                  label: room.tertiaryActionLabel,
+                  url: room.tertiaryActionUrl
+                },
+              });
+            }),
+            description: entries.firstSectionHeroDescription
+          },
+          roomCollection: entries.roomCollection.map(entry => {
+            const room = entry.fields;
+            return {
+              title: room.title,
+              keyValues: [room.squareFeet, room.bedType, room.maxGuests],
+              description: room.shortDescription.content[0].content[0].value,
+              image: {
+                url: room.cardImageUrl,
+              },
+              primaryAction: {
+                label: room.primaryActionLabel,
+                url: room.primaryActionUrl,
+              },
+              secondaryAction: {
+                label: room.secondaryActionLabel,
+                url: '/',
+              },
+              tertiaryAction: {
+                label: room.tertiaryActionLabel,
+                url: room.tertiaryActionUrl,
+              },
+            };
+         }),
+          roomCollectionLayout: entries.roomCollectionLayout,
+          secondHero: {
+            premium: true,
+            title: entries.secondSectionHeroTitle,
+            images: entries.secondSectionHeroRooms.map((entry) => {
+              const room = entry.fields;
+              return ({
+                url: room.cardImageUrl,
+                caption: 'needs content field',
+                tertiaryAction: {
+                  label: room.tertiaryActionLabel,
+                  url: room.tertiaryActionUrl
+                }, 
+              });
+            }),
+            description: entries.secondSecondHeroDescription,
+            secondaryAction: {
+              label: entries.secondSectionHeroSecondaryActionLabel,
+              url: entries.secondSectionHeroSecondaryActionUrl,
+            }
+          },
+          promoSection: {
+            title: entries.promoSectionTitle,
+            button: {
+              label: entries.promoSectionButtonLabel,
+              url: entries.promoSectionButtonUrl,
+            },
+            cards: entries.promoCards.map(promoCard => {
+            const card = promoCard.fields;
+              return {
+                imageUrl: card.imageUrl,
+                categoryName: card.categoryName,
+                title: card.title,
+                description: card.description,
+                status: {
+                  color: card.statusColor ? card.statusColor : '',
+                  label: card.statusLabel,
+                },
+                primaryAction: {
+                  label: card.primaryActionLabel,
+                  url: card.primaryActionUrl,
+                },
+                tertiaryAction: {
+                  label: card.tertiaryActionLabel,
+                  url: card.tertiaryActionUrl,
+                },
+              };
+            })
+          },
+        };
     });
 
-    {
-      /* Todo: input validation, either here or Contentful */
-    }
-    return {
-      textHeader: entries.items[0].fields.textHeader.fields,
-      hero: entries.items[0].fields.sectionHero.fields,
-      roomCollection: entries.items[0].fields.roomCollection.map(room => {
-        return room.fields;
-      }),
-      roomCollectionLayout: entries.items[0].roomCollectionLayout,
-      secondHero: entries.items[0].fields.premiumSectionHero.fields,
-      promoSection: {
-        title: entries.items[0].fields.promoSectionTitle,
-        button: {
-          label: entries.items[0].fields.promoSectionButtonLabel,
-          url: entries.items[0].fields.promoSectionButtonUrl,
-        },
-        cards: entries.items[0].fields.promoCards.map(card => {
-          return card.fields;
-        }),
-      },
-    };
+    return pageComponents;
   }
 
   createTextHeader(data) {
     return <G2TextHeaderSection title={data.title} subtitle={data.subtitle} />;
   }
-  createHero(data, imageCount) {
-    // GraphQL needed for proper Contentful implementation of Hero
-    const images = [];
-    for (let i = 1; i < imageCount + 1; i++) {
-      console.log(i);
-      images.push({
-        url: data[`room${i}imageUrl`],
-        caption: data[`room${i}caption`],
-        tertiaryAction: {
-          label: data.tertiaryActionLabel,
-          url: data[`room${i}actionUrl`],
-        },
-      });
-    }
+  
+  createHero(data) {
     return (
       <G2HeroSection
         title={data.title}
         premium={data.premium}
-        description={data.description.content[0].content[0].value}
-        images={images}
+        description={data.description}
+        images={data.images}
+        secondaryAction={data.secondaryAction}
       />
     );
   }
   createRoomOverview(collection, layout) {
     return (
       <G2RoomOverviewCardCollectionSection
-        rooms={collection.map(room => {
-          return {
-            title: room.title,
-            keyValues: [room.squareFeet, room.bedType, room.maxGuests],
-            description: room.shortDescription.content[0].content[0].value,
-            image: {
-              url: room.cardImageUrl,
-            },
-            primaryAction: {
-              label: room.primaryActionLabel,
-              url: room.primaryActionUrl,
-            },
-            secondaryAction: {
-              label: room.secondaryActionLabel,
-              url: '/' + room.slug,
-            },
-            tertiaryAction: {
-              label: room.tertiaryActionLabel,
-              url: room.tertiaryActionUrl,
-            },
-          };
-        })}
+        rooms={collection}
         layout={layout}
       />
     );
@@ -104,26 +149,7 @@ class Index extends React.Component {
           label: data.button.label,
           url: data.button.url,
         }}
-        cards={data.cards.map(card => {
-          return {
-            imageUrl: card.imageUrl,
-            categoryName: card.categoryName,
-            title: card.title,
-            description: card.description,
-            status: {
-              color: card.statusColor ? card.statusColor : '',
-              label: card.statusLabel,
-            },
-            primaryAction: {
-              label: card.primaryActionLabel,
-              url: card.primaryActionUrl,
-            },
-            tertiaryAction: {
-              label: card.tertiaryActionLabel,
-              url: card.tertiaryActionUrl,
-            },
-          };
-        })}
+        cards={data.cards}
       />
     );
   }
@@ -132,7 +158,7 @@ class Index extends React.Component {
     // This approach allows to reorder the components depending on data coming from the CMS
     const components = [
       this.createTextHeader(this.props.textHeader),
-      this.createHero(this.props.hero, 3),
+      this.createHero(this.props.hero),
       this.createRoomOverview(
         this.props.roomCollection,
         this.props.roomCollectionLayout,
@@ -141,7 +167,11 @@ class Index extends React.Component {
       this.createPromoSection(this.props.promoSection),
     ];
 
-    return <Layout>{components.map(component => component)}}</Layout>;
+    return (
+      <Layout>
+        {components.map(component => component)}
+      </Layout>
+    );
   }
 }
 
