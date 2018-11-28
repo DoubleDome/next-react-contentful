af0ff9437e40a882bd5b1ff77a2b7614c1ac3c75
Cristian Abello <cristian.abello@valpo.edu>
2018-11-28 17:51:02 +0000

    new keys

--- next/integrations/contentful/config.js
+++ next/integrations/contentful/config.js
@@ -9,16 +9,16 @@ module.exports = {
         '0e3f3d2838d483c6176991c783ff529d1c3e4163188d1b09690229be63c2f318',
     },
     rooms: {
-      id: 'bdtysnvzs3h8',
+      id: 'yoh2vcslqwwa',
       preview:
-        '015009d6997c04268680e42da919d5d369357a9bd925d4a696f60aa69387b39f',
+        '9fe247b61621364d2477336486d69374583b64d3bc73ee657cd9c9e168c57f60',
       delivery:
-        'cdf1047b43a2276fae263f23b3fabc384e4a7f9493d9e68cd917309a748ecda5',
+        '80f27f878fb85e062cdfc0de979cd4ac2e7fc4ba347f6b3743ba059e2ffbdd01',
     },
   },
   management: {
     common:
-      'CFPAT-4a4d298652f1a26738726342b3bf35f04db9eda15bce4608cd3c41aa539eadd9',
+      'CFPAT-35025baaf2329876e2b1e1f67bd21b888329b0b581205a9e4d5d9d9df6eae1c0',
   },
   hosts: {
     preview: 'preview.contentful.com',
