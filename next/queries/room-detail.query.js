45738aa9e0794ba5dacc73acec255aecb87ec8de
Cristian Abello <cristian.abello@valpo.edu>
2018-12-04 19:17:53 +0000

    removed now-dead query field

--- next/queries/room-detail.query.js
+++ next/queries/room-detail.query.js
@@ -2,120 +2,118 @@ module.exports = {
     gqlQuery(id) {
      return( 
         `   
-query roomDetailPageQuery {
-  roomDetailPageCollection(where: {slug: "${id}"}) {
-    items {
-      ... on RoomDetailPage {
-        componentsListCollection {
-          items {
-            ... on ComponentPlacement {
-              component {
-                name
-              }
-              dataField
-            }
-          }
-        }
-        overviewHeaderPrimaryActionLabel
-        overviewHeaderPrimaryActionUrl
-        overviewHeaderSecondaryActionLabel
-        overviewHeaderSecondaryActionUrl
-        overviewBodyPrimaryActionLabel
-        overviewBodyPrimaryActionUrl
-        overviewBodyTertiaryActionLabel
-        overviewBodyTertiaryActionUrl
-        sidebarSectionHeadlineTitle
-        sidebarSectionHeadlineText
-        sidebarSectionHeadlineText
-        highlightCarouselTitle
-        twoColumnHeroTitle
-        twoColumnHeroImageUrl
-        twoColumnHeroSubtitle
-        twoColumnHeroBody
-        twoColumnHeroActionLink
-        twoColumnHeroSidebarHeadline
-        similarRoomsSectionTitle
-        similarRoomsActionText
-        twoColumnHeroSidebarContent
-        accordionTitle
-        contact {
-          ... on ContactInformation {
-            phoneNumber
-            emailAddress
-          }
-        }
-        room {
-          subtitle
-          secondaryCopy
-          brand
-          cardImageUrl
-          title
-          squareFeet
-          bedType
-          maxGuests
-          otherAmenitiesCollection {
-            items {
-              ... on RoomAmenity {
-                title
-                itemList
-              }
-            }
-          }
-          featuredAmenitiesCollection {
-            items {
-              ... on RoomAmenity {
-                title
-              }
-            }
-          }
-          unifiedGalleryCollection {
-            items {
-              description
-              imageUrl
-            }
-          }
-          highlightsCollection {
-            items {
-              title
-              description
-              imageUrl
-            }
-          }
-          galleryImageUrls
-          longDescription {
-            json
-          }
-          lounge {
-            title
-            loungeHours
-          }
-          similarRoomsCollection(limit: 1) {
-            items {
-              ... on Room {
-                squareFeet
-                bedType
-                title
-                maxGuests
-                shortDescription {
-                  json
-                }
-                cardImageUrl
-                linkedFrom {
-                  roomDetailPageCollection {
+          query roomDetailPageQuery {
+            roomDetailPageCollection(where: {slug: "${id}"}) {
+              items {
+                ... on RoomDetailPage {
+                  componentsListCollection {
                     items {
-                      slug
+                      ... on ComponentPlacement {
+                        component {
+                          name
+                        }
+                        dataField
+                      }
+                    }
+                  }
+                  overviewHeaderPrimaryActionLabel
+                  overviewHeaderPrimaryActionUrl
+                  overviewHeaderSecondaryActionLabel
+                  overviewHeaderSecondaryActionUrl
+                  overviewBodyPrimaryActionLabel
+                  overviewBodyPrimaryActionUrl
+                  overviewBodyTertiaryActionLabel
+                  overviewBodyTertiaryActionUrl
+                  sidebarSectionHeadlineTitle
+                  sidebarSectionHeadlineText
+                  sidebarSectionHeadlineText
+                  highlightCarouselTitle
+                  twoColumnHeroTitle
+                  twoColumnHeroImageUrl
+                  twoColumnHeroSubtitle
+                  twoColumnHeroBody
+                  twoColumnHeroActionLink
+                  twoColumnHeroSidebarHeadline
+                  similarRoomsSectionTitle
+                  similarRoomsActionText
+                  twoColumnHeroSidebarContent
+                  accordionTitle
+                  contact {
+                    ... on ContactInformation {
+                      phoneNumber
+                      emailAddress
+                    }
+                  }
+                  room {
+                    subtitle
+                    brand
+                    cardImageUrl
+                    title
+                    squareFeet
+                    bedType
+                    maxGuests
+                    otherAmenitiesCollection {
+                      items {
+                        ... on RoomAmenity {
+                          title
+                          itemList
+                        }
+                      }
+                    }
+                    featuredAmenitiesCollection {
+                      items {
+                        ... on RoomAmenity {
+                          title
+                        }
+                      }
+                    }
+                    unifiedGalleryCollection {
+                      items {
+                        description
+                        imageUrl
+                      }
+                    }
+                    highlightsCollection {
+                      items {
+                        title
+                        description
+                        imageUrl
+                      }
+                    }
+                    galleryImageUrls
+                    longDescription {
+                      json
+                    }
+                    lounge {
+                      title
+                      loungeHours
+                    }
+                    similarRoomsCollection(limit: 1) {
+                      items {
+                        ... on Room {
+                          squareFeet
+                          bedType
+                          title
+                          maxGuests
+                          shortDescription {
+                            json
+                          }
+                          cardImageUrl
+                          linkedFrom {
+                            roomDetailPageCollection {
+                              items {
+                                slug
+                              }
+                            }
+                          }
+                        }
+                      }
                     }
                   }
                 }
               }
             }
           }
-        }
-      }
-    }
-  }
-}
-
       `
     );
   },
